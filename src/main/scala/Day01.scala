eff4a730b7813e655d6836edbc8600d63bd84e7b
nmcb <marco@splatter.nl>
2023-04-12 11:36:45 +0200

    solved day 01 part 02

--- src/main/scala/Day01.scala
+++ src/main/scala/Day01.scala
@@ -17,3 +17,18 @@ object Day01 extends App:
 
   val answer1: Int = frequencies.sum
   println(s"Answer day $day part 1: ${answer1} [${System.currentTimeMillis - start1}ms]")
+
+  def firstFrequencyFoundTwice(frequencies: List[Int]): Int =
+    assert(frequencies.length >= 2, "contains less than 2 frequencies")
+
+    def find(fs: List[Int], seen: Set[Int] = Set(0), acc: Int = 0): Int =
+      fs match
+        case Nil                              => find(frequencies, seen, acc)
+        case h :: _ if seen.contains(h + acc) => h + acc
+        case h :: t                           => find(t, seen + (h + acc), h + acc)
+
+    find(frequencies)
+
+  val answer2: Int = firstFrequencyFoundTwice(frequencies)
+    println(s"Answer day $day part 2: ${answer2} [${System.currentTimeMillis - start1}ms]")
+
