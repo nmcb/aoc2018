891d5a3fe748ac71db4bffaa83c101b87ca8bf67
nmcb <marco@splatter.nl>
2023-04-15 08:16:26 +0200

    solved day 7 part 1

---
+++ src/main/scala/Day07.scala
@@ -0,0 +1,40 @@
+import scala.io.*
+import scala.annotation.*
+import scala.collection.immutable.SortedMap
+
+object Day07 extends App:
+
+  val day: String =
+    this.getClass.getName.drop(3).init
+
+  def ttsort[A](edges: List[(A, A)])(using Ordering[A]): List[A] =
+
+    @tailrec
+    def tsort(deps: SortedMap[A, Set[A]], done: List[A] = List.empty): List[A] =
+      val (nodeps, hasdeps) = deps.partition(_._2.isEmpty)
+      if nodeps.isEmpty && hasdeps.isEmpty then done else
+        val found  = nodeps.keys.headOption.getOrElse(sys.error("edges contains cycle"))
+        val ignore = nodeps.drop(1)
+        val next   = hasdeps.map((node,ds) => node -> (ds - found)) ++ ignore
+        tsort(next, done :+ found)
+
+    tsort(edges.foldLeft(SortedMap[A, Set[A]]())((ds, e) =>
+      ds + (e._1 -> ds.getOrElse(e._1, Set())) + (e._2 -> (ds.getOrElse(e._2, Set()) + e._1))
+    ))
+
+  val steps: List[(Char,Char)] =
+
+    def parser(s: String): (Char,Char) =
+      s match
+        case s"Step $fst must be finished before step $snd can begin." => (fst.head, snd.head)
+        case _ => sys.error("boom!")
+
+    Source
+      .fromResource(s"input$day.txt")
+      .getLines
+      .map(parser)
+      .toList
+
+  val start1: Long = System.currentTimeMillis
+  val answer1: String = ttsort(steps).mkString("")
+  println(s"Answer day $day part 1: ${answer1} [${System.currentTimeMillis - start1}ms]")
--- src/test/scala/TestSolutions.scala
+++ src/test/scala/TestSolutions.scala
@@ -10,7 +10,10 @@ class TestSolutions extends AnyFunSuite:
     assertResult(5368)(actual = Day02.answer1)
     assertResult("cvgywxqubnuaefmsljdrpfzyi")(actual = Day02.answer2)
   }
-  ignore("Day03 [15s]") {
+  test("Day03 [15s]") {
     assertResult(107043)(actual = Day03.answer1)
     assertResult(346)(actual = Day03.answer2)
+  }
+  test("Day07 [15s]") {
+    assertResult("FHICMRTXYDBOAJNPWQGVZUEKLS")(actual = Day07.answer1)
   }
