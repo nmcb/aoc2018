2b79cb06c927bed120ad6bae236c56e0130472f2
nmcb <marco@splatter.nl>
2023-04-15 08:20:26 +0200

    naming

--- src/main/scala/Day07.scala
+++ src/main/scala/Day07.scala
@@ -7,18 +7,18 @@ object Day07 extends App:
   val day: String =
     this.getClass.getName.drop(3).init
 
-  def ttsort[A](edges: List[(A, A)])(using Ordering[A]): List[A] =
+  def tsort[A](edges: List[(A, A)])(using Ordering[A]): List[A] =
 
     @tailrec
-    def tsort(deps: SortedMap[A, Set[A]], done: List[A] = List.empty): List[A] =
+    def loop(deps: SortedMap[A, Set[A]], done: List[A] = List.empty): List[A] =
       val (nodeps, hasdeps) = deps.partition(_._2.isEmpty)
       if nodeps.isEmpty && hasdeps.isEmpty then done else
         val found  = nodeps.keys.headOption.getOrElse(sys.error("edges contains cycle"))
         val ignore = nodeps.drop(1)
         val next   = hasdeps.map((node,ds) => node -> (ds - found)) ++ ignore
-        tsort(next, done :+ found)
+        loop(next, done :+ found)
 
-    tsort(edges.foldLeft(SortedMap[A, Set[A]]())((ds, e) =>
+    loop(edges.foldLeft(SortedMap[A, Set[A]]())((ds, e) =>
       ds + (e._1 -> ds.getOrElse(e._1, Set())) + (e._2 -> (ds.getOrElse(e._2, Set()) + e._1))
     ))
 
@@ -36,5 +36,5 @@ object Day07 extends App:
       .toList
 
   val start1: Long = System.currentTimeMillis
-  val answer1: String = ttsort(steps).mkString("")
+  val answer1: String = tsort(steps).mkString("")
   println(s"Answer day $day part 1: ${answer1} [${System.currentTimeMillis - start1}ms]")
