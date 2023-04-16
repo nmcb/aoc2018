c806ed31dabff13a877996ed4cc132f6a80be7c5
nmcb <marco@splatter.nl>
2023-04-16 18:56:30 +0200

    solved day 07 part 2

--- src/test/scala/TestSolutions.scala
+++ src/test/scala/TestSolutions.scala
@@ -6,7 +6,7 @@ class TestSolutions extends AnyFunSuite:
     assertResult(477)(actual = Day01.answer1)
     assertResult(390)(actual = Day01.answer2)
   }
-  test("Day02 []") {
+  test("Day02 [130ms]") {
     assertResult(5368)(actual = Day02.answer1)
     assertResult("cvgywxqubnuaefmsljdrpfzyi")(actual = Day02.answer2)
   }
@@ -14,6 +14,7 @@ class TestSolutions extends AnyFunSuite:
     assertResult(107043)(actual = Day03.answer1)
     assertResult(346)(actual = Day03.answer2)
   }
-  test("Day07 [15s]") {
+  test("Day07 [39ms]") {
     assertResult("FHICMRTXYDBOAJNPWQGVZUEKLS")(actual = Day07.answer1)
+    assertResult(946)(actual = Day07.answer2)
   }
