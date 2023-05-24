146b5d0b0c275370ac2311ca58b436ea348a4106
nmcb <marco@splatter.nl>
2023-05-24 11:34:23 +0200

    solved day 9

--- src/test/scala/TestSolutions.scala
+++ src/test/scala/TestSolutions.scala
@@ -34,3 +34,7 @@ class TestSolutions extends AnyFunSuite:
     assertResult(47244)(actual = Day08.answer1)
     assertResult(17267)(actual = Day08.answer2)
   }
+  test("Day09 [2100ms]") {
+    assertResult(412117)(actual = Day09.answer1)
+    assertResult(3444129546L)(actual = Day09.answer2)
+  }
