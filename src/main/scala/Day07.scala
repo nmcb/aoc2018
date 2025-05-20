de7d4cf81259adeda2cfe15e0c35d306be5147e6
nmcb <nmcb@xs4all.nl>
2025-05-20 18:12:27 +0200

    cleanup day 07

--- src/main/scala/Day07.scala
+++ src/main/scala/Day07.scala
@@ -7,24 +7,26 @@ object Day07 extends App:
   val day: String =
     this.getClass.getName.drop(3).init
 
-  def solve[A](edges: List[(A,A)], timer: A => Int, parallelization: Int)(using Ordering[A]): (Int,List[A]) =
+  def solve[A](edges: Vector[(A,A)], timer: A => Int, parallelization: Int)(using Ordering[A]): (Int,Vector[A]) =
 
     case class Work(queue: SortedMap[A,Int] = SortedMap(), workers: SortedMap[A,Int] = SortedMap()):
 
       def isDone: Boolean =
         queue.isEmpty && workers.isEmpty
 
-      def tick(schedule: List[A]): (List[A], Work) =
+      def tick(schedule: Vector[A]): (Vector[A], Work) =
         val work = queue ++ schedule.map(a => a -> timer(a))
         val (add, left)  = work.splitAt(parallelization - workers.size)
         val (done, todo) = (workers ++ add).map((c,t) => (c, t - 1)).partition((_,t) => t == 0)
-        (done.keys.toList, Work(queue = left, workers = todo))
+        (done.keys.toVector, Work(queue = left, workers = todo))
 
     @tailrec
-    def loop(dependencies: SortedMap[A,Set[A]], time: Int = 0, work: Work = Work(), done: List[A] = List()): (Int, List[A]) =
-      if dependencies.isEmpty && work.isDone then (time, done) else
+    def loop(dependencies: SortedMap[A,Set[A]], time: Int = 0, work: Work = Work(), done: Vector[A] = Vector()): (Int, Vector[A]) =
+      if dependencies.isEmpty && work.isDone then
+        (time, done)
+      else
         val (nodeps, hasdeps) = dependencies.partition((_,ds) => ds.isEmpty)
-        val (processed, working) = work.tick(nodeps.keys.toList)
+        val (processed, working) = work.tick(nodeps.keys.toVector)
         val next = hasdeps.map((node,ds) => node -> (ds -- processed))
         loop(next, time + 1, working, done :++ processed)
 
@@ -32,7 +34,7 @@ object Day07 extends App:
       ds + (e._1 -> ds.getOrElse(e._1, Set())) + (e._2 -> (ds.getOrElse(e._2, Set()) + e._1))
     ))
 
-  val steps: List[(Char,Char)] =
+  val steps: Vector[(Char,Char)] =
 
     def parser(s: String): (Char,Char) =
       s match
@@ -43,15 +45,12 @@ object Day07 extends App:
       .fromResource(s"input$day.txt")
       .getLines
       .map(parser)
-      .toList
+      .toVector
 
-  val start1: Long = System.currentTimeMillis
-  val answer1: String = solve(steps, timer = _ => 1, parallelization = 1)._2.mkString("")
-  println(s"Day $day answer day $day part 1: ${answer1} [${System.currentTimeMillis - start1}ms]")
-  assert(answer1 == "FHICMRTXYDBOAJNPWQGVZUEKLS")
+  val start1  = System.currentTimeMillis
+  val answer1 = solve(steps, timer = _ => 1, parallelization = 1)._2.mkString("")
+  println(s"Day $day answer day $day part 1: $answer1 [${System.currentTimeMillis - start1}ms]")
 
-
-  val start2: Long = System.currentTimeMillis
-  val answer2: Int = solve(steps, timer = _.toInt - 4, parallelization = 5)._1
+  val start2       = System.currentTimeMillis
+  val (answer2, _) = solve(steps, timer = _.toInt - 4, parallelization = 5)
   println(s"Day $day answer day $day part 2: ${answer2} [${System.currentTimeMillis - start2}ms]")
-  assert(answer2 == 946)
