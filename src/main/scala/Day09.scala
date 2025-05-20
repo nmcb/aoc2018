0fd9df07c92a90fcfff82e0ad5023a8069946734
nmcb <nmcb@xs4all.nl>
2025-05-20 18:18:51 +0200

    cleanup day 09

--- src/main/scala/Day09.scala
+++ src/main/scala/Day09.scala
@@ -10,21 +10,17 @@ object Day09 extends App:
 
     private def next: Circle[A] =
       tail match
-        case hd :: tl =>
-          Circle(current :: init, hd, tl)
-        case Nil =>
-          init.reverse match
-            case hd :: it => Circle(List(current), hd, it)
-            case Nil      => this
+        case hd :: tl => Circle(current :: init, hd, tl)
+        case Nil      => init.reverse match
+                           case hd :: it => Circle(List(current), hd, it)
+                           case Nil      => this
 
     private def prev: Circle[A] =
       init match
-        case hd :: it =>
-          Circle(it, hd, current :: tail)
-        case Nil =>
-          tail.reverse match
-            case hd :: tl => Circle(tl, hd, List(current))
-            case Nil      => this
+        case hd :: it => Circle(it, hd, current :: tail)
+        case Nil      => tail.reverse match
+                           case hd :: tl => Circle(tl, hd, List(current))
+                           case Nil      => this
 
     @tailrec
     final def rotate(n: Int): Circle[A] =
@@ -39,30 +35,29 @@ object Day09 extends App:
         case Nil      => (current, Circle(init.tail, init.head, tail))
 
   object Circle:
-    def apply[A](elem: A): Circle[A] = Circle(Nil, elem, Nil)
+    def apply[A](elem: A): Circle[A] =
+      Circle(Nil, elem, Nil)
 
 
   def play(players: Int, last: Int): Long =
-
     @tailrec
-    def loop(marbles: Circle[Int], marble: Int, player: Int, scores: Map[Int, Long]): Map[Int, Long] =
+    def go(marbles: Circle[Int], marble: Int, player: Int, scores: Map[Int, Long]): Map[Int, Long] =
 
       val p: Int = player % players + 1
 
       if marble > last then
         scores
       else if marble % 23 != 0 then
-        loop(marbles.rotate(2).insert(marble), marble + 1, p, scores)
+        go(marbles.rotate(2).insert(marble), marble + 1, p, scores)
       else
         val (r, next) = marbles.rotate(-7).removed
         val s = scores.updated(player, scores(player) + marble + r)
-        loop(next, marble + 1, p, s)
+        go(next, marble + 1, p, s)
 
-    loop(Circle(0), 1, 1, Map.empty.withDefaultValue(0)).values.max
+    go(Circle(0), 1, 1, Map.empty.withDefaultValue(0)).values.max
 
   def solve(players: Int, last: Int, multiplier: Int = 1): Long =
     play(players, last * multiplier)
-
 
   val (players, points) =
     Source.fromResource(s"input$day.txt").mkString.trim match
